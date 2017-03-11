39d4433a1335bd117f0a947632e556ad742efb8c
WitoldSlawko <witold.slawko@gmail.com>
2017-03-11 15:47:50 +0100

    gifList and GifItem-s added

--- react-gif-search/src/index.js
+++ react-gif-search/src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/SearchBar.js';
+import GifList from './components/GifList';
 
 class App extends React.Component {
+  
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      gifs: [
+        {
+          id: 1,
+          url: 'http://fakeimg.pl/300/'
+        },
+        {
+          id: 2,
+          url: 'http://fakeimg.pl/300/'
+        },
+        {
+          id: 3,
+          url: 'http://fakeimg.pl/300/'
+        }
+      ]
+    }
+  }
   
   handleTermChange(term){
     console.log(term);
@@ -12,6 +34,7 @@ class App extends React.Component {
     return (
       <div className="greeting">
         <SearchBar onTermChange={this.handleTermChange} />
+        <GifList gifs={this.state.gifs} />
       </div>
     );
   }
